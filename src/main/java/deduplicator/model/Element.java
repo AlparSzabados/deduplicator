511e26a0aee43f55daa200a2717b001c29e0fd5b
Alpar <szabadosalpar@gmail.com>
2020-01-19 11:24:52 +0200

    Replaces Test Pojo with real implementation

--- src/main/java/deduplicator/model/Element.java
+++ src/main/java/deduplicator/model/Element.java
@@ -1,10 +1,16 @@
 package deduplicator.model;
 
-// TODO replace with real code
+import java.nio.file.Path;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
 public class Element {
     public String name;
-    public String lastName;
-    public String email;
-    public String phone;
-    public String active;
+    public List<String> locations;
+
+    public Element(List<Path> duplicates) {
+        this.name = duplicates.get(0).toString();
+        this.locations = duplicates.stream().map(Path::toString).collect(toList());
+    }
 }
