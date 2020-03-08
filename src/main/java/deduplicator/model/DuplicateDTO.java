a9cb57450441abfd1a2eb1ed870a55e9a79c9e75
Alpar <szabadosalpar@gmail.com>
2020-03-08 12:16:21 +0200

    Added DTO

---
+++ src/main/java/deduplicator/model/DuplicateDTO.java
@@ -0,0 +1,31 @@
+package deduplicator.model;
+
+import javax.persistence.*;
+import java.nio.file.Path;
+import java.util.List;
+
+@Table(name = "duplicates")
+@Entity
+public class DuplicateDTO {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public String id;
+
+    @Column(name = "size")
+    public String size;
+
+    @Column(name = "name")
+    public String name;
+
+    @Column(name = "path")
+    public String path;
+
+    @Column(name = "start_bytes")
+    public String startBytes;
+
+    @Column(name = "checksum")
+    public String checkSum;
+
+}
